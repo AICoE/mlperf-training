90ba9abe14bc6135617f146303ac5dc425b7a104
raynalee4 <rlbirdo14@gmail.com>
2020-02-07 09:50:49 -0500

    fixing syntax

--- image_classification/tensorflow/pipeline/pipeline_setup.sh
+++ image_classification/tensorflow/pipeline/pipeline_setup.sh
@@ -1,6 +1,6 @@
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/resources.yml
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/buildah.yaml
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/run.yml
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/pipeline.yml
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/pipelinerun.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/resources.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/buildah.yaml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/run.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/pipeline.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/pipelinerun.yml
 
