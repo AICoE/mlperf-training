da3128a1245cd55a84910e8f5dfd53a439c52c92
raynalee4 <rlbirdo14@gmail.com>
2020-02-07 08:53:17 -0500

    set file location to online

--- image_classification/tensorflow/pipeline/pipeline_setup.sh
+++ image_classification/tensorflow/pipeline/pipeline_setup.sh
@@ -1,6 +1,6 @@
-oc apply -f resources.yml
-oc apply -f buildah.yaml
-oc apply -f run.yml
-oc apply -f pipeline.yml
-oc apply -f pipelinerun.yml
+oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/resources.yml
+oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/buildah.yaml
+oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/run.yml
+oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/pipeline.yml
+oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/pipelinerun.yml
 
