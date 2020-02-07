66eec29462b10297f919099289623ff933edfaa9
raynalee4 <rlbirdo14@gmail.com>
2020-02-07 09:32:07 -0500

    trying yaml formatted

--- image_classification/tensorflow/pipeline/pipeline_setup.sh
+++ image_classification/tensorflow/pipeline/pipeline_setup.sh
@@ -1,6 +1,6 @@
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/resources.yml
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/buildah.yaml
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/run.yml
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/pipeline.yml
-oc apply -f https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/pipelinerun.yml
+oc apply -f --yaml https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/resources.yml
+oc apply -f --yaml https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/buildah.yaml
+oc apply -f --yaml https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/run.yml
+oc apply -f --yaml https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/pipeline.yml
+oc apply -f --yaml https://github.com/raynalee4/training/blob/master/image_classification/tensorflow/pipeline/pipelinerun.yml
 
