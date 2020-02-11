caa231327f285614e36a51afeba68034e3547329
Rayna Levinson <rlevinso@localhost.localdomain>
2020-02-11 16:52:52 -0500

    fix files

--- data_generation/fractal_graph_expansions/pipeline/pipeline_setup.sh
+++ data_generation/fractal_graph_expansions/pipeline/pipeline_setup.sh
@@ -1,6 +1,6 @@
 oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/resources.yml
 oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/buildah.yaml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/run.yml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline.yml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipelinerun.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/run.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/pipeline.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/pipelinerun.yml
 
