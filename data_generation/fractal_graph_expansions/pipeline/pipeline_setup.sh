af89a7536b8fb54a3f9be51715fe789dbff138da
Rayna Levinson <rlevinso@localhost.localdomain>
2020-02-11 16:52:00 -0500

    fix files

--- data_generation/fractal_graph_expansions/pipeline/pipeline_setup.sh
+++ data_generation/fractal_graph_expansions/pipeline/pipeline_setup.sh
@@ -1,6 +1,6 @@
 oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/resources.yml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/buildah.yaml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/run.yml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/pipeline.yml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/image_classification/tensorflow/pipeline/pipelinerun.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/buildah.yaml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/run.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline.yml
+oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipelinerun.yml
 
