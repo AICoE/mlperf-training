70b240741948e99976caa57daec52a9ab2484020
Rayna Levinson <rlevinso@localhost.localdomain>
2020-02-12 10:47:10 -0500

    renaming

--- data_generation/fractal_graph_expansions/pipeline/pipeline_drop.sh
+++
@@ -1,6 +0,0 @@
-oc delete pipeline/image-classification
-oc delete pipelinerun/tf-build-pipeline-run
-oc delete pipelineresource/build-image
-oc delete pipelineresource/repo
-oc delete task/buildah
-oc delete task/run
--- data_generation/fractal_graph_expansions/pipeline/pipeline_setup.sh
+++
@@ -1,6 +0,0 @@
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/resources.yml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/buildah.yaml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/run.yml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/pipeline.yml
-oc create -f https://raw.githubusercontent.com/raynalee4/training/master/data_generation/fractal_graph_expansions/pipeline/pipelinerun.yml
-
