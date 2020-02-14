32018a36d3e1f6bcf0af603d0a462104ee4bd53d
Rayna Levinson <rlevinso@localhost.localdomain>
2020-02-14 11:16:04 -0500

    its not building anymore

--- data_generation/fractal_graph_expansions/restart_crc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-crc delete
-crc setup
-~/Documents/crc_start_instructions.sh
-oc login -u kubeadmin -p cznQP-n4pBk-cnXTg-nkevH https://api.crc.testing:6443
-oc new-project mlperf
-oc create -f pipeline/imagestream.yml
-
